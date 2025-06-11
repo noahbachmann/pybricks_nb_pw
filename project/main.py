f6ec4cd9dbc2836b5d40f036bae94ba7f30d1eb4
noahbachmann <noah.bachmann@kauz.ch>
2025-06-11 15:54:49 +0200

    feat: change it all

--- project/main.py
+++ project/main.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env pybricks-micropython
+#Noah Bachmann - Paul Willimann
 
-from pybricks.hubs import EV3Brick
-from pybricks.parameters import Button, Color
 from pybricks.hubs import EV3Brick
 from pybricks.tools import wait
 from pybricks.ev3devices import Motor, UltrasonicSensor, ColorSensor
-from pybricks.parameters import Port, Stop
+from pybricks.parameters import Port, Stop, Color
 from pybricks.robotics import DriveBase
-from math import pi
 
-# ------------------------------------------------------------------------
-# Initialisierung
-# ------------------------------------------------------------------------
 ev3 = EV3Brick()
-
 middle_motor = Motor(Port.A)         
 left_motor   = Motor(Port.B)              
 right_motor  = Motor(Port.C)               
@@ -22,6 +16,7 @@ color = ColorSensor(Port.S4)
 
 # screenWidth = 178
 # screenHeight = 128
+#9.5cm pro 100 fahrt
 
 wheel_diameter = 56  # Durchmesser der Räder in mm (EV3 Standard: ~56mm)
 axle_track = 114     # Spurbreite zwischen den Rädern in mm (z.B. 114mm)
@@ -30,14 +25,14 @@ drive_base = DriveBase(left_motor, right_motor,
 
 if __name__ == "__main__":
     distance = 0
-    middle_motor.run(100)
+    middle_motor.run(400)
     while(infrared.distance(True) > 120):
         drive_base.straight(100)
         distance += 100
         if color.color() == Color.BROWN:
             break
         elif color.color() == Color.BLACK:
-            ev3.screen.load_image("black_image.jpg")
+            ev3.screen.load_image("black_image.png")
             ev3.speaker.play_file("black_sound.wav")
         elif color.color() == Color.GREEN:
             ev3.screen.load_image("green_image.png")
@@ -45,30 +40,30 @@ if __name__ == "__main__":
         elif color.color() == Color.RED:
             ev3.light.on(Color.RED)
             ev3.screen.load_image("red_image.png")
-            drive_base.drive(0, 500)
-            middle_motor.run(250)
+            middle_motor.run(900)
+            drive_base.drive(0, 100)
             wait(3000)
             drive_base.stop()
-            drive_base.drive(0, -500)
+            drive_base.drive(0, -100)
             wait(3000)
             drive_base.stop()
-            middle_motor.run(100)
+            middle_motor.run(400)
             ev3.light.off()
         elif color.color() == Color.WHITE:
-            ev3.screen.load_image("white_image.jpg")
+            ev3.screen.load_image("white_image.png")
             ev3.speaker.play_file("white_sound.wav")
         elif color.color() == Color.BLUE:
-            ev3.screen.load_image("blue_image.jpg")
+            ev3.screen.load_image("blue_image.png")
             ev3.speaker.play_file("blue_sound.wav")
         elif color.color() == Color.YELLOW:
-            ev3.screen.load_image("yellow_image.jpg")
+            ev3.screen.load_image("yellow_image.png")
             ev3.speaker.play_file("yellow_sound.wav")
         else:
-            pass
+            continue
 
         wait(2000)
     
-    ev3.screen.load_image("end.jpg")
+    ev3.screen.load_image("end.png")
     middle_motor.stop(Stop.HOLD)
     drive_base.turn(180)    
     drive_base.straight(distance)
